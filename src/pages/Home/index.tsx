463b1b85b979f9cfd6b2ba28a84600955901b521
Leonardo Cunha <lalmcunha@gmail.com>
2020-06-11 16:43:31 -0300

    get uf e uf combo ok, buscando senadores

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
@@ -6,7 +7,78 @@ import './styles.css';
 import Header from '../../components/Header';
 import SenatorCard from '../../components/SenatorCard'
 
+interface IBGEUFResponse {
+    sigla: string;
+    nome: string;
+}
+
+interface SenadorListaResponse {
+    ListaParlamentarEmExercicio: {
+        Parlamentares: {
+            Parlamentar: [
+                {
+                    IdentificacaoParlamentar: {
+                        CodigoParlamentar: number;
+                        NomeParlamentar: string;
+                        NomeCompletoParlamentar: string;
+                        UrlFotoParlamentar: string;
+                        UrlPaginaParlamentar: string;
+                        EmailParlamentar: string;
+                        SiglaPartidoParlamentar: string;
+                        UfParlamentar: string;
+                        MembroMesa: string;
+                        MembroLideranca: string;
+                    }
+                }
+            ]
+        }
+    }
+}
+interface UF {
+    initials: string;
+    name: string;
+}
+
+interface Senator {
+    name: string;
+    party: string;
+    img: string;
+}
+
 const Home = () => {
+    const [ ufs, setUfs ] = useState<UF[]>([]);
+    const [ filteredUf, setFilteredUf ] = useState('0');
+    const [ senators, setSenators ] = useState<Senator[]>([]);
+
+    /**
+     * Carregando UFs da API do IBGE
+     */
+    useEffect( () => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then( response => {
+            const ufs = response.data.map( registro => ({ initials: registro.sigla , name: registro.nome }));
+            setUfs(ufs);
+        });
+    }, []);
+
+    /**
+     * Buscando lista de senadores em exercício
+     */
+    useEffect(()=>{
+        axios.get<SenadorListaResponse>('http://legis.senado.leg.br/dadosabertos/senador/lista/atual.json').then( response => {
+            console.log(response.data.ListaParlamentarEmExercicio.Parlamentares.Parlamentar  ); // <- array
+        })
+    }, []);
+
+    function handleUfChange(event: ChangeEvent<HTMLSelectElement>) {
+        const ufToFilter = event.target.value;
+        setFilteredUf(ufToFilter);
+    }
+    
+    /**
+     * TODO: sempre que mudar o filtro de UF, atualizar um array que tem os senadores filtrados e renderizar esse array apenas
+     */
+
+
     return (
         <>
             <Header />
@@ -15,8 +87,13 @@ const Home = () => {
                     <div className="form-group">
                         <select name="uf"
                                 id="uf"
-                                className="form-control">
+                                className="form-control"
+                                value={filteredUf}
+                                onChange={handleUfChange}>
                             <option value="0">Selecione um Estado</option>
+                            {ufs.map( uf =>
+                                (<option key={uf.initials} value={uf.initials}> {`${uf.name} - ${uf.initials}`} </option>)
+                            )}
                         </select>
                     </div>
                     <div className="form-group">
@@ -24,6 +101,7 @@ const Home = () => {
                                 id="partido"
                                 className="form-control">
                             <option value="0">Selecione um Partido</option>
+                            
                         </select>
                     </div>
                 </div>
